8edde530e7a94ec6cd8570c43e9744de48853175
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-04-10 08:09:57 +0300

    Added Category Controller

---
+++ src/main/java/name/nicholasgribanov/spring5webluxrest/controllers/CategoryController.java
@@ -0,0 +1,30 @@
+package name.nicholasgribanov.spring5webluxrest.controllers;
+
+import name.nicholasgribanov.spring5webluxrest.domain.Category;
+import name.nicholasgribanov.spring5webluxrest.repository.CategoryRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class CategoryController {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryController(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @GetMapping("/api/v1/categories")
+    Flux<Category> list() {
+        return categoryRepository.findAll();
+    }
+
+    @GetMapping("/api/v1/categories/{id}")
+    Mono<Category> getById(@PathVariable String id) {
+        return categoryRepository.findById(id);
+
+    }
+}
