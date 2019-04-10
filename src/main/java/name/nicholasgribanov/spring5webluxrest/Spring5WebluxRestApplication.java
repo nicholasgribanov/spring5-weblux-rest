2e6ffbb9b33767eda2d20132896d37a4b43915f1
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-04-10 07:40:04 +0300

    Initial commit

---
+++ src/main/java/name/nicholasgribanov/spring5webluxrest/Spring5WebluxRestApplication.java
@@ -0,0 +1,13 @@
+package name.nicholasgribanov.spring5webluxrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebluxRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5WebluxRestApplication.class, args);
+    }
+
+}
